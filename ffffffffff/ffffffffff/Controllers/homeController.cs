81a7070c225cc3bd583b015ed97535134a248145
jani <jani@gmail.com>
2022-08-23 10:34:19 +0530

    second

---
+++ ffffffffff/ffffffffff/Controllers/homeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ffffffffff.Controllers
+{
+    public class homeController : Controller
+    {
+        // GET: home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult cc()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
